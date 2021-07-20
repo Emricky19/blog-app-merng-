ce15a67fbca9ca994300727d6bca65207b4ec7fc
Emrick19 <emricky17@gmail.com>
2021-07-20 20:47:05 +0100

    Add a space

--- util/check-auth.js
+++ util/check-auth.js
@@ -7,7 +7,7 @@ module.exports = (context) => {
     const authHeader = context.req.headers.authorization;
     if(authHeader){
         const token = authHeader.split('Bearer ')[1];
-        if(token){
+        if(token){ 
             try{
                 const user = jwt.verify(token, TOKEN_SECRET);
                 return user;
